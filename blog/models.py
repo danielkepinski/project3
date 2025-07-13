0b86c6405fa57890ed765c5a00317574e96d9dc9
danielkepinski <l397waws@students.codeinstitute.net>
2025-07-13 19:30:39 +0100

    created superuser aka admin

--- blog/models.py
+++ blog/models.py
@@ -1,18 +1,35 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 class Post(models.Model):
- title = models.CharField(max_length=250)
- slug = models.SlugField(max_length=250)
- body = models.TextField()
- publish = models.DateTimeField(default=timezone.now)
- created = models.DateTimeField(auto_now_add=True)
- updated = models.DateTimeField(auto_now=True)
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
 
- class Meta:
- ordering = ['-publish']
- indexes = [
- models.Index(fields=['-publish']),
- ]
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_posts',
+        default=1  # This will use the user with ID 1 (or any other valid user ID)
+    )
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status,
+        default=Status.DRAFT
+    )
 
- def __str__(self):
- return self.t
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+
+    def __str__(self):
+        return self.title
