fbf642a58c9a0242492e0b090abf344220444a39
danielkepinski <l397waws@students.codeinstitute.net>
2025-07-14 18:08:37 +0100

    updated url.py for comments

--- blog/url.py
+++ blog/url.py
@@ -4,14 +4,20 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    # Post list view
-    path('', views.post_list, name='post_list'),
+    # Post list view (using class-based view)
     path('', views.PostListView.as_view(), name='post_list'),
 
-    # Post detail view with date and slug
+    # Post detail view
     path(
         '<int:year>/<int:month>/<int:day>/<slug:post>/',
         views.post_detail,
         name='post_detail'
     ),
+
+    # Post share view
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
+
+    # Post comment submission view
+    path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
 ]
+
