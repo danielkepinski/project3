16b665770c8335faf557d5f164006e2b0b44749d
danielkepinski <l397waws@students.codeinstitute.net>
2025-07-15 20:05:26 +0100

    added markdown to detail.html

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -2,7 +2,6 @@ from django import template
 from blog.models import Post
 from django.db.models import Count
 import markdown
-from django import template
 from django.utils.safestring import mark_safe
 
 register = template.Library()
