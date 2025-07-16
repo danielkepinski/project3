8e2c08dbef9618d8158293ebe4a8041ccdaff81b
danielkepinski <l397waws@students.codeinstitute.net>
2025-07-16 12:11:28 +0100

    made a trigram extention

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,11 +86,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
+        'NAME':   config('DB_NAME'),
+        'USER':   config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),  
-        'PORT': config('DB_PORT'),   
+        'HOST':   config('DB_HOST', default='localhost'),
+        'PORT':   config('DB_PORT', default='5432'),
     }
 }
 
