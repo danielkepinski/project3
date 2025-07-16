6d2df04e2e732fafd076dafe54b03562959377b3
danielkepinski <l397waws@students.codeinstitute.net>
2025-07-16 16:13:17 +0100

    added class postform and commentform in forms.py

--- blog/models.py
+++ blog/models.py
@@ -7,9 +7,7 @@ from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return (
-            super().get_queryset().filter(status=Post.Status.PUBLISHED)
-        )
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
 
 class Post(models.Model):
@@ -18,56 +16,31 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(
-        max_length=250,
-        unique_for_date='publish'
-    )
-    author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name='blog_posts'
-    )
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(
-        max_length=2,
-        choices=Status,
-        default=Status.DRAFT
-    )
+    status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
 
-    objects = models.Manager()  # The default manager.
-    published = PublishedManager()  # Our custom manager.
+    objects = models.Manager()  # Default manager
+    published = PublishedManager()  # Custom manager
     tags = TaggableManager()
 
     class Meta:
         ordering = ['-publish']
-        indexes = [
-            models.Index(fields=['-publish']),
-        ]
+        indexes = [models.Index(fields=['-publish'])]
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse(
-            'blog:post_detail',
-            args=[
-                self.publish.year,
-                self.publish.month,
-                self.publish.day,
-                self.slug,
-            ],
-        )
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(
-        Post,
-        on_delete=models.CASCADE,
-        related_name='comments'
-    )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
@@ -77,9 +50,7 @@ class Comment(models.Model):
 
     class Meta:
         ordering = ['created']
-        indexes = [
-            models.Index(fields=['created']),
-        ]
+        indexes = [models.Index(fields=['created'])]
 
     def __str__(self):
         return f'Comment by {self.name} on {self.post}'
