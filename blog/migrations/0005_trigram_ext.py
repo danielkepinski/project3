69eb58f26100ee0d9de9de802f20dc364a7d1543
danielkepinski <l397waws@students.codeinstitute.net>
2025-07-16 12:54:22 +0100

    having issues with migrations so fixed file names, also created do.sh docker compose and dockerfile

--- blog/migrations/0005_trigram_ext.py
+++ blog/migrations/0005_trigram_ext.py
@@ -7,8 +7,9 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('blog', '0005_post_tags'),
-    ]
+    ('blog', '0004_post_tags'),
+]
+
 
     operations = [
         TrigramExtension()
