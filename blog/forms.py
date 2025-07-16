959f414cb3c0fc059dbfb401426f7995f48b1d07
danielkepinski <l397waws@students.codeinstitute.net>
2025-07-16 12:31:43 +0100

    fixed some indentation within blog folder

--- blog/forms.py
+++ blog/forms.py
@@ -1,16 +1,23 @@
 from django import forms
+
 from .models import Comment
+
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
-    to = forms.EmailField(label="Recipient Email")
-    comments = forms.CharField(required=False, widget=forms.Textarea)
+    to = forms.EmailField()
+    comments = forms.CharField(
+        required=False,
+        widget=forms.Textarea
+    )
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'email', 'body']
 
+
 class SearchForm(forms.Form):
-    query = forms.CharField(label='Search', max_length=100)
+    query = forms.CharField()
