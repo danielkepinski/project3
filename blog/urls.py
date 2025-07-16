1d3a271f2d45a8883beb310f56af5ec37f80f29a
danielkepinski <l397waws@students.codeinstitute.net>
2025-07-16 12:54:22 +0100

    having issues with migrations so fixed file names, also created do.sh docker compose and dockerfile

--- blog/urls.py
+++ blog/urls.py
@@ -1,31 +1,26 @@
 from django.urls import path
+
 from . import views
 from .feeds import LatestPostsFeed
 
 app_name = 'blog'
 
 urlpatterns = [
-    # Post list view (using class-based view)
-    path('', views.PostListView.as_view(), name='post_list'),  # This should be for the homepage (blog index)
-
-    # Post list filtered by tag
-    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),  # Post list filtered by tag
-
-    # Post detail view
+    # Post views
+    path('', views.post_list, name='post_list'),
+    # path('', views.PostListView.as_view(), name='post_list'),
+    path(
+        'tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'
+    ),
     path(
         '<int:year>/<int:month>/<int:day>/<slug:post>/',
         views.post_detail,
-        name='post_detail'
+        name='post_detail',
     ),
-
-    # Post share view
     path('<int:post_id>/share/', views.post_share, name='post_share'),
-
-    # Post comment submission view
-    path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
-
-    path('feed/', LatestPostsFeed(), name='post_feed'),  # RSS feed for latest posts
+    path(
+        '<int:post_id>/comment/', views.post_comment, name='post_comment'
+    ),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
+    path('search/', views.post_search, name='post_search'),
 ]
-# blog/urls.py
-path('<int:post_id>/share/', views.post_share, name='post_share'),
-
