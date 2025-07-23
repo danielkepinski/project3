bdf21092966cd52b4f40f583c9689fadd3ac6a43
danielkepinski <l397waws@students.codeinstitute.net>
2025-07-23 14:23:44 +0100

    commented out links in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -89,16 +89,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-https://docs.djangoproject.com/en/5.0/topics/i18n/
+    #https://docs.djangoproject.com/en/5.0/topics/i18n/
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-https://docs.djangoproject.com/en/5.0/howto/static-files/
+#https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+#https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email settings
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
