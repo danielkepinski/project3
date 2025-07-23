0bf247d954b9263c9df2b17bdd8f2be59e33e528
danielkepinski <l397waws@students.codeinstitute.net>
2025-07-23 15:37:48 +0100

    Enable DEBUG for error debugging

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,9 +16,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
+DEBUG = config('DEBUG', 'ALLOWED_HOSTS', default=False, cast=bool)
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com',]
 SITE_ID = 1
 # Application definition
 INSTALLED_APPS = [
