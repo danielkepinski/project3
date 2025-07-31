fee7f328f1681a4f096841ab9715ff2c6a6f503b
danielkepinski <l397waws@students.codeinstitute.net>
2025-07-31 21:02:43 +0100

    added more to my readme

--- blog/urls.py
+++ blog/urls.py
@@ -1,43 +1,26 @@
-from django.urls import path, include
+from django.urls import path
 from django.contrib import admin
 from . import views
 from .feeds import LatestPostsFeed
 from .forms import CommentForm
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Post  # <-- you must import Post for add_comment
-
-
-# Add comment view (you might want to keep or move this to views.py)
-def add_comment(request, post_pk):
-    post = get_object_or_404(Post, pk=post_pk)
-    if request.method == 'POST':
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.post = post
-            comment.save()
-            return redirect(post)
-    else:
-        form = CommentForm()
-    # You should return a response in the 'else' case, example:
-    return render(request, 'your_comment_form_template.html', {'form': form})
+from .models import Post
 
 
 app_name = 'blog'
 
-
 urlpatterns = [
-    # Admin
+    # Admin interface
     path('admin/', admin.site.urls),
 
-    # Homepage
-    path('', views.home, name='blog-home'),
+    # Homepage and post list
+    path('', views.post_list, name='blog-home'),         # Homepage
+    path('posts/', views.post_list, name='post_list'),   # For tests and direct nav
 
-    # Post List (used for testing and tagged views)
-    path('posts/', views.post_list, name='post_list'),
+    # Tag filtering
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
 
-    # Post Detail and Actions
+    # Post detail and actions
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
