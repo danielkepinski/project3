00c0193e18a020da88253cb34cffb031c28d9fd1
danielkepinski <l397waws@students.codeinstitute.net>
2025-08-07 22:11:07 +0100

    Fix missing bracket in blog/urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,24 +1,22 @@
-from django.urls import path, include
-from django.contrib import admin
+from django.urls import path
 from . import views
 from .feeds import LatestPostsFeed
-from .forms import CommentForm
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Post
-
 
 app_name = 'blog'
 
 urlpatterns = [
-    urlpatterns = [
-    path('', views.post_list, name='blog-home'),
-    path('posts/', views.post_list, name='post_list'),
-    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+    path('', views.post_list, name='blog-home'),  # Homepage
+    path('posts/', views.post_list, name='post_list'),  # All posts
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),  # Posts by tag
 
+    # Post detail and actions
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
 
+    # RSS feed
     path('feed/', LatestPostsFeed(), name='post_feed'),
+
+    # Search
     path('search/', views.post_search, name='post_search'),
 ]
