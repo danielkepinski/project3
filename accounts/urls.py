16f8bbc0dd6a8d59fd466476b2953f0046ff2a7a
danielkepinski <l397waws@students.codeinstitute.net>
2025-08-07 20:48:11 +0100

    added login to settings

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
-from .views import register
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('register/', register, name='register'),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),  
+    path('accounts/', include('django.contrib.auth.urls')),  
+    path('', include('blog.urls')),  
 ]
