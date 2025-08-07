38371e3c69fe8ad412103fb7a001a6b7e5ef42c0
danielkepinski <l397waws@students.codeinstitute.net>
2025-08-07 22:50:13 +0100

    Make comment.user optional to support existing data

--- blog/models.py
+++ blog/models.py
@@ -47,11 +47,8 @@ class Comment(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name='comments'
-    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments', null=True, blank=True)
+
 
     class Meta:
         ordering = ['created']
